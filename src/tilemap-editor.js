f370a784ac0f5e61705278173b1fad44ce5029f9
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-12 15:58:50 -0400

    Line 306 Move grid show button

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -303,7 +303,7 @@
 
             <div style="margin-top: 5px; justify-items: center; text-align: left; font-size: medium;" id="toolButtonsWrapper" class="tool_wrapper"> 
 
-<button style="text-align: center; cursor: pointer; margin-top: 10px; margin-bottom: 15px; color: gold; height: 24px; border-radius: 6px; width: 136px; font-size: 14px;"><label style="cursor: pointer;" for="tool2" title="↔️ move and scroll around the map ⊞" data-value="2">◀  Move Map  ▶</label></button>
+<button style="text-align: center; cursor: pointer; margin-top: 10px; margin-bottom: 15px; color: gold; height: 24px; border-radius: 6px; width: 110px; font-size: 14px;"><label style="cursor: pointer;" for="tool2" title="↔️ move and scroll around the map ⊞" data-value="2"><small>◀  </small>Move Map<small>  ▶</small></label></button> <input title="Show grid above" value="true" checked="checked" type="checkbox" id="showGrid" checked />
 
                     
                       <input id="tool0" type="radio" value="0" name="tool" checked class="hidden"/>
@@ -349,7 +349,7 @@
       <div style="display: flow !important; width: 180px; background-color: transparent;" id="mapSelectContainer" class="tilemaps_selector">
 
                         <a style="display: none; visibility: hidden; cursor: help; text-decoration: none;" title="Settings and grid options" class="button" href="#popup1">⚙️</a>
-                        <input style="display: none; visibility: hidden;" title="Show grid above" value="true" checked="checked" type="checkbox" id="showGrid" checked />
+                        
             
             <div style="display: none; visibility: hidden;" id="popup1" class="overlay">
             <div class="popup">
