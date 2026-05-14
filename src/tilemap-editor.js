904c90f9852153833b239cca50a798d5a61a3598
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-14 18:07:51 -0400

    Update save json btns line 1643

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -1640,7 +1640,7 @@
             transformer: exportImage
         }
         apiTileMapExporters.saveData = {
-            name: `save json`,
+            name: "save json",
             description: "save map as a json file to edit later",
             transformer: exportJson
         }
@@ -1654,7 +1654,7 @@
         //}
         apiTileMapImporters = tileMapImporters;
         apiTileMapImporters.openData = {
-            name: `upload json`,
+            name: "upload json",
             description: "upload / open map json file to continue editing a map",
             onSelectFiles: (setData, files) => {
                 const readFile = new FileReader();
