2525409db383a2450ed77ea13d35f5b6a64a1ffa
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-21 16:06:04 -0400

    Update 345px to 350px

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -119,7 +119,7 @@
 
       </div>
       <div class="card_body">
-        <div id="card_left_column_alt" class="card_left_column" style="background: #353540; width: 345px;">
+        <div id="card_left_column_alt" class="card_left_column" style="background: #353540; width: 350px;">
         
         <details style="display: none; visibility: hidden;" class="details_container sticky_left" id="tilesetDataDetails" open="true">
           <summary >
