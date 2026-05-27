3e7ecbbb73a7bf3eab56e74b04c82a529658b97e
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-27 10:25:34 -0400

    Revert button two order swap

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -176,13 +176,13 @@
                         <div class="tab-container">
                           <div style="background: linear-gradient(to bottom, #28423a 94%, aliceblue 94%);" class="tabs">
                             <button value=0 style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;background-color: aliceblue;color: #252c39;font-weight: bolder;font-size: medium;border-bottom: none !important;border-color:  transparent;/* outline-color: transparent; */border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;" 
-							onclick="document.getElementById('mapholder').style.display = 'table' ; document.getElementById('tileGroupButtons').style.display = 'block' ; document.getElementById('card_left_column_alt').scrollTo({ left: 0, top: this.value});"
+							onclick="document.getElementById('mapholder').style.display = 'table' ; document.getElementById('card_left_column_alt').scrollTo({ left: 0, top: this.value}) ; document.getElementById('tileGroupButtons').style.display = 'block'"
 
 							class="tab active" data-tab="home" id="myTiles">Tiles 📚</button>
                             <button style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;border-color: transparent;background: paleturquoise;color: #283442;font-size: medium;font-weight: bold;border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;"
-							onclick="document.getElementById('tileGroupButtons').style.display = 'none';" 
+							onclick="document.getElementById('mapholder').style.display = 'none';" onclick="document.getElementById('tileGroupButtons').style.display = 'none';" 
 							onmouseup="document.getElementById('myTiles').value = document.getElementById('card_left_column_alt').scrollTop;" 
-							onmousedown="document.getElementById('mapholder').style.display = 'none';" onclick="document.getElementById('tileGroupButtons').style.display = 'none';"
+							onmousedown=" document.getElementById('tileGroupButtons').style.display = 'none';"
 							class="tab" data-tab="about" id="myMaps">Maps 🌄</button>
                           </div>
 
