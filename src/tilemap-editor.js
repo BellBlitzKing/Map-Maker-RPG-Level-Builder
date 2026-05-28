12bb6753219fb88ec925f341c1999ecbfff4f994
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-28 02:36:14 -0400

    Update start color

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -853,7 +853,7 @@
                 const tileSize = tileSets[tilesetIdx]?.tileSize || SIZE_OF_CROP;
 
                 if(!(tilesetIdx in TILESET_ELEMENTS)) { //texture not found
-                    ctx.fillStyle = '#7e674c'; // this was red, set initial block color
+                    ctx.fillStyle = '#735b42'; // this was red, set initial block color
                     ctx.fillRect(positionX * SIZE_OF_CROP * ZOOM, positionY * SIZE_OF_CROP * ZOOM, SIZE_OF_CROP * ZOOM, SIZE_OF_CROP * ZOOM);
                     return;
                 }
