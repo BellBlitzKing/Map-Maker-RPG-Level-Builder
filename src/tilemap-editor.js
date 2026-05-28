aee1dc1087bb8538c7860d457ec257165f6804e4
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-28 00:58:20 -0400

    Test 1 - tileSelectStart = [1]

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -807,7 +807,7 @@
         })
     }
 
-    let tileSelectStart = 1;
+    let tileSelectStart = [1];
     const getSelectedTile = (event) => {
         const { x, y } = event.target.getBoundingClientRect();
         const tileSize = tileSets[tilesetDataSel.value].tileSize * ZOOM;
