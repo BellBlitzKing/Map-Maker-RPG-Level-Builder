e75746f1a379273090523b6ebc387ab391c6c17a
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-28 23:57:12 -0400

    Revert save - deleting files made no difference
    Deleting multiple files to save space in caching. Error still occurs.
    
    Failed to save state QuotaExceededError: Failed to execute 'setItem' on 'Storage': Setting the value of 'tilemapEditorState' exceeded the quota.
        at saveStateToLocalStorage (tilemap-editor.js:2323:18)
        at apiOnUpdateCallback (tilemap-editor.js:1744:13)
        at onUpdateState (tilemap-editor.js:1381:9)
        at updateSelection (tilemap-editor.js:751:8)
        at HTMLDivElement.<anonymous> (tilemap-editor.js:1814:13)

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -160,12 +160,12 @@
                             <button value=0 style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;background-color: aliceblue;color: #252c39;font-weight: bolder;font-size: medium;border-bottom: none !important;border-color:  transparent;/* outline-color: transparent; */border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;" 
 							onclick="document.getElementById('mapholder').style.display = 'table' ; document.getElementById('card_left_column_alt').scrollTo({ left: 0, top: this.value}) ; document.getElementById('tileGroupButtons').style.display = 'block'"
 
-							class="tab active" data-tab="home" id="myTiles">Tilex 📚</button>
+							class="tab active" data-tab="home" id="myTiles">Tiles 📚</button>
                             <button style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;border-color: transparent;background: paleturquoise;color: #283442;font-size: medium;font-weight: bold;border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;"
 							onclick="document.getElementById('mapholder').style.display = 'none';" onclick="document.getElementById('tileGroupButtons').style.display = 'none';" 
 							onmouseup="document.getElementById('myTiles').value = document.getElementById('card_left_column_alt').scrollTop;" 
 							onmousedown=" document.getElementById('tileGroupButtons').style.display = 'none';"
-							class="tab" data-tab="about" id="myMaps">Mapx 🌄</button>
+							class="tab" data-tab="about" id="myMaps">Maps 🌄</button>
                           </div>
 
                 <div style="cursor: default; margin-top: 8px; margin-bottom: 20px; padding-bottom: 10px;" id="tileGroupButtons">
@@ -501,7 +501,7 @@
     }
 
     const getSnappedPos = (pos) => (Math.round(pos / (SIZE_OF_CROP)) * (SIZE_OF_CROP));
-    let selection = [];
+    let selection = 1;
     let currentLayer = 1;
     let isMouseDown = false;
     let maps = {};
@@ -1172,7 +1172,6 @@
 
     const exportImage = () => {
         draw(false);
-		document.getElementById("showGrid").checked = false;
         const data = canvas.toDataURL();
         const image = new Image();
         image.src = data;
@@ -1180,7 +1179,6 @@
         const w = window.open('');
         w.document.write(image.outerHTML);
         draw();
-		document.getElementById("showGrid").checked = true;
     }
 
     const getTilesAnalisis = (ctx, width, height, sizeOfTile) =>{
@@ -1685,15 +1683,6 @@
             appState
         }
     ) => {
-        let savedState;
-        try {
-            savedState = localStorage.getItem('State');
-            if (savedState) {
-                appState = JSON.parse(savedState);
-            }
-        } catch (e) {
-            console.warn('Failed to load saved state', e);
-        }
         // Attach
         const attachTo = document.getElementById(attachToId);
         if(attachTo === null) return;
@@ -1738,13 +1727,8 @@
             },
             acceptFile: "application/JSON"
         }
-        // apiOnUpdateCallback = onUpdate;
-        apiOnUpdateCallback = (...args) => {
-            onUpdate(...args);
-            saveStateToLocalStorage();
-        };
-        exports.onUpdate = apiOnUpdateCallback;
-		
+        apiOnUpdateCallback = onUpdate;
+
         if(onMouseUp){
             apiOnMouseUp = onMouseUp;
             document.getElementById('tileMapEditor').addEventListener('pointerup', function(){
@@ -2306,25 +2290,19 @@
             SHOW_GRID = appState.SHOW_GRID;
         }
         
-				        // Animated tiles when on frames mode
-				        const animateTiles = () => {
-				            if (tileDataSel.value === "frames") draw();
-				            requestAnimationFrame(animateTiles);
-				        }
-				        requestAnimationFrame(animateTiles);
-				    };
+        // Animated tiles when on frames mode
+        const animateTiles = () => {
+            if (tileDataSel.value === "frames") draw();
+            requestAnimationFrame(animateTiles);
+        }
+        requestAnimationFrame(animateTiles);
+    };
+
+
     
     exports.getState = () => {
         return getAppState();
     }
-	    const saveStateToLocalStorage = () => {
-	        try {
-	            const state = getAppState();
-				localStorage.setItem('tilemapEditorState', JSON.stringify(state));
-	        } catch (e) {
-	            console.warn('Failed to save state', e);
-	        }
-	    };
 
     exports.onUpdate = apiOnUpdateCallback;
     exports.onMouseUp = apiOnMouseUp;
