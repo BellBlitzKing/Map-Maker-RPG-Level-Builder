4bff713901da7a3c61a56c06c909361a55d0b024
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-05-28 23:29:14 -0400

    Update tilemap-editor.js

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -160,12 +160,12 @@
                             <button value=0 style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;background-color: aliceblue;color: #252c39;font-weight: bolder;font-size: medium;border-bottom: none !important;border-color:  transparent;/* outline-color: transparent; */border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;" 
 							onclick="document.getElementById('mapholder').style.display = 'table' ; document.getElementById('card_left_column_alt').scrollTo({ left: 0, top: this.value}) ; document.getElementById('tileGroupButtons').style.display = 'block'"
 
-							class="tab active" data-tab="home" id="myTiles">Tilez 📚</button>
+							class="tab active" data-tab="home" id="myTiles">Tilex 📚</button>
                             <button style="height: 32px; width: 125px;margin-top: 10px;border-top-left-radius: 12px;border-top-right-radius: 12px;border-color: transparent;background: paleturquoise;color: #283442;font-size: medium;font-weight: bold;border-bottom-left-radius: 0px;border-bottom-right-radius: 0px;"
 							onclick="document.getElementById('mapholder').style.display = 'none';" onclick="document.getElementById('tileGroupButtons').style.display = 'none';" 
 							onmouseup="document.getElementById('myTiles').value = document.getElementById('card_left_column_alt').scrollTop;" 
 							onmousedown=" document.getElementById('tileGroupButtons').style.display = 'none';"
-							class="tab" data-tab="about" id="myMaps">Mapz 🌄</button>
+							class="tab" data-tab="about" id="myMaps">Mapx 🌄</button>
                           </div>
 
                 <div style="cursor: default; margin-top: 8px; margin-bottom: 20px; padding-bottom: 10px;" id="tileGroupButtons">
@@ -1172,6 +1172,7 @@
 
     const exportImage = () => {
         draw(false);
+		document.getElementById("showGrid").checked = false;
         const data = canvas.toDataURL();
         const image = new Image();
         image.src = data;
@@ -1179,6 +1180,7 @@
         const w = window.open('');
         w.document.write(image.outerHTML);
         draw();
+		document.getElementById("showGrid").checked = true;
     }
 
     const getTilesAnalisis = (ctx, width, height, sizeOfTile) =>{
